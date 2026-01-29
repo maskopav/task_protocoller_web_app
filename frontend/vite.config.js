1198d6b722da4afbc1b3f9bfa5cf38b5be1f470a
maskopav <maskopav@fel.cvut.cz>
2026-01-29 13:23:15 +0100

    fixed testing on the samrtphone using secure connection, plus installing the  @mediapipe/tasks-vision for camera tests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      basicSsl()
+    ],
     base: env.VITE_APP_BASE_PATH || '/',
+    server: {
+      host: true,
+      https: true, // Force HTTPS
+    }
   };
 });
